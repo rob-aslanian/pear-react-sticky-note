4c81c48ca31aec48b03a1632b21e7932c4f9f76e
rob-aslanian <roborobert16@gmail.com>
2025-07-23 19:06:46 +0400

    fixed small lint issues

--- src/layouts/EmptyState.js
+++ src/layouts/EmptyState.js
@@ -6,19 +6,27 @@ const { Title, Text } = Typography;
 
 export default () => {
   return html`
-    <div
-      className="center-content"
-      style=${{
-        color: "#999",
-        textAlign: "center",
-      }}
+   <div
+    className="center-content"
+    style=${{
+      color: "#999",
+      textAlign: "center",
+    }}
     >
-      <${FileTextOutlined} style=${{
-    fontSize: 48,
-    marginBottom: 16,
-  }} />
-      <${Title} level=${4}>No sticky notes</${Title}>
-      <${Text} type="secondary">Click the "Add Note" button to create one.</${Text}>
+      <${FileTextOutlined}
+        style=${{
+          fontSize: 48,
+          marginBottom: 16,
+        }}
+      />
+      
+      <${Title} level=${4}>
+        No sticky notes
+      </${Title}>
+
+      <${Text} type="secondary">
+        Click the "Add Note" button to create one.
+      </${Text}>
     </div>
   `;
 };
